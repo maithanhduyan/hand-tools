e4510c9b86da8d5fcce982a629b533a2aacbb3dd
Mai Thành Duy An <tiachop0102@gmail.com>
2022-11-11 14:11:52 +0900

    [feature-web] Template integration.

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Web.Controllers
             return View();
         }
 
+        public IActionResult AboutUS()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
